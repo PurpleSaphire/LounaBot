dd26a2955a74845f80a7e18663255876e33398b1
JulioRaven <114471308+JulioRaven@users.noreply.github.com>
2024-02-18 10:17:25 +0100

    Add files via upload

--- scripts/cmds/hercai.js
+++ scripts/cmds/hercai.js
@@ -1,84 +1,60 @@
+/*
+    hercai release v1.2:
+        • hercai port for GoatBot v2
+        • parseJSON add
+        • dont remove the author pls, an credit would help.
+        • Easy Setup
+        • Fixed by LiANE, salamat po!
+        
+    MIT License ruihq 2023:
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Setup here!
+const apiLink = "https://hercai.onrender.com/v2/hercai?question="
+const BOTPREFIX = "YOURBOTPREFIX";
+const BOTNAME = "YOURBOTNAME";
 const axios = require("axios");
 
+// module ixports
 module.exports = {
   config: {
-    name: "HercAI",
-    aliases: ['hercai'],
-    version: "1.0",
-    author: "Xyron Chen | Ace",
-    countDown: 10,
+    name: "hercai",
+    version: "1.2",
+    author: "Rui and LiANE",
+    countDown: 5,
     role: 0,
-    shortDescription: {
-      vi: "",
-      en: "Ask HercAI anything."
-    },
-    longDescription: {
-      vi: "",
-      en: "Ask Anything you want on HercAI."
-    },
-    category: "ai - chat",
-    guide: {
-      en: "   {pn} <prompt>"
-    }
+    shortDescription: { vi: "", en: "hercai command" },
+    longDescription: { vi: "", en: "Hercai command, used to chat with the hercai model." },
+    category: "chatbot",
+    guide: { vi: "", en: "{pn} <your chat>" }
   },
-  
-  onStart: async function({ message, event, args, commandName }) {
-    const prompt = args.join(' ');
-
-    try {
-      const ace = await message.reply(`Hercai répond... Patientez un instant.`);
-      message.reaction("🤔", event.messageID, () => {}, true);
-      message.reaction("💜", ace.messageID, () => {}, true);
-
-      const response = await axios.get(
-        `https://openai-rest-api.vercel.app/hercai?ask=${encodeURIComponent(prompt)}&model=v3`
-      );
-
-      if (response.data && response.data.reply) {
-        const answer = response.data.reply;
-        const sagot = `Herc ( AI )\n\n${answer}`;
-        message.reply({ body: sagot }, (err, info) => {
-          global.GoatBot.onReply.set(info.messageID, {
-            commandName,
-            messageID: info.messageID,
-            author: event.senderID
-          });
-        });
-      } 
-      message.unsend(ace.messageID);
-
-    } catch (error) {
-      console.error("Error:", error.message);
+  // Logic here!
+  onStart: async function({ api, args, event, message }) {
+    const response = args.join(" ");
+    if (args.length === 0) {
+      message.reply("Provide a question or query.");
+      return;
     }
-  },
-
-  onReply: async function({ message, event, Reply, args }) {
-    let { author, commandName } = Reply;
-    if (event.senderID != author) return;
-    const text = args.join(' ');
+    message.reply(`Finding an answer for: ${response}...`);
     try {
-      const hintay = await message.send(`Hercai répond... Patientez un instant.`);
-      message.reaction("🤔", event.messageID, () => {}, true);
-      message.reaction("💜", hintay.messageID, () => {}, true);
-      const response = await axios.get(
-        `https://openai-rest-api.vercel.app/hercai?ask=${encodeURIComponent(text)}&model=v3`
-      );
+      const res = await axios.get(`${apiLink}${response}`);
+      const responseData = res.data;
 
-      if (response.data && response.data.reply) {
-        const answer = response.data.reply;
-        const sagot2 = `Herc ( AI )\n\n${answer}`;
-        message.reply({ body: sagot2 }, (err, info) => {
-          global.GoatBot.onReply.set(info.messageID, {
-            commandName,
-            messageID: info.messageID,
-            author: event.senderID
-          });
-        });
+      // parseJSON (ruihq)
+      if (typeof responseData === "object" && responseData.hasOwnProperty("content") && responseData.hasOwnProperty("reply")) {
+        const { content, reply } = responseData;
+        message.reply(`[Herc.ai]: ${reply}\n\n[ ${BOTPREFIX} | ${BOTNAME} ]`);
+      } else {
+        message.reply("Invalid response format received from the API.");
       }
-      message.unsend(hintay.messageID);
-
     } catch (error) {
-      console.error("Error:", error.message);
+      console.error(error);
+      message.reply("An error occurred while fetching the response.");
     }
   }
 };
--- scripts/cmds/imagine.js
+++ scripts/cmds/imagine.js
@@ -1,79 +1,45 @@
 const axios = require('axios');
+const fs = require('fs');
+const { get } = require('request-promise');
+
 module.exports = {
   config: {
-    name: "imagine",
-    version: "1.0",
-    author: "rehat--",
+    name: 'imagine',
+    aliases: [],
+    version: '1.0',
+    author: 'kshitiz',
     countDown: 10,
-    longDescription: {
-      en: "Create an image from your text with 4 models like midjourney."
-    },
-    category: "ai",
     role: 0,
-    guide: {
-      en: '1 | DreamshaperXL10' +
-        '\n2 | DynavisionXL' +
-        '\n3 | JuggernautXL' +
-        '\n4 | RealismEngineSDXL' +
-        '\n5 | Sdxl 1.0'
+    shortDescription: 'Generate an image.',
+    longDescription: 'Generate an image.',
+    category: 'fun',
+    guide: '{pn}[prompt | model]',
+  },
+
+  onStart: async function ({ api, args, message, event }) {
+    let path = __dirname + '/cache/image.png';
+    const tzt = args.join(' ').split('|').map(item => item.trim());
+    let txt = tzt[0];
+    let txt2 = tzt[1];
+
+    let tid = event.threadID;
+    let mid = event.messageID;
+
+    if (!args[0] || !txt || !txt2) {
+      return api.sendMessage('Please provide a prompt and a model.', tid, mid);
     }
-  },
-
-  onStart: async function ({ api, event, args, message }) {
-    const info = args.join(' ');
-    const [promptPart, modelPart] = info.split('|').map(item => item.trim());
-
-    if (!promptPart) return message.reply("Add something baka.");
-
-    message.reply("Please wait...⏳", async (err, info) => {
-      let ui = info.messageID;
-
-      try {
-        const modelParam = modelPart;
-        let apiUrl = `https://turtle-apis.onrender.com/api/v2/sdxl?prompt=${encodeURIComponent(promptPart)}`;
-        if (modelPart) {
-          apiUrl += `&model=${modelParam}`;
-        }
-
-        const response = await axios.get(apiUrl);
-        const combinedImg = response.data.combinedImage;
-        const img = response.data.imageUrls.image;
-        message.unsend(ui);
-        message.reply({
-          body: "Please reply with the image number (1, 2, 3, 4) to get the corresponding image in high resolution.",
-          attachment: await global.utils.getStreamFromURL(combinedImg)
-        }, async (err, info) => {
-          let id = info.messageID; global.GoatBot.onReply.set(info.messageID, {
-            commandName: this.config.name,
-            messageID: info.messageID,
-            author: event.senderID,
-            imageUrls: response.data.imageUrls
-          });
-        });
-      } catch (error) {
-        console.error(error);
-        api.sendMessage(`${error}`, event.threadID);
-      }
-    });
-  },
-
-  onReply: async function ({ api, event, Reply, usersData, args, message }) {
-    const reply = parseInt(args[0]);
-    const { author, messageID, imageUrls } = Reply;
-
-    if (event.senderID !== author) return;
 
     try {
-      if (reply >= 1 && reply <= 4) {
-        const img = imageUrls[`image${reply}`];
-        message.reply({ attachment: await global.utils.getStreamFromURL(img) });
-      } else {
-        message.reply("❌ | Invalid number try again later.");
-      }
-    } catch (error) {
-      console.error(error);
-      message.reply(`${error}`, event.threadID);
+      api.sendMessage('⏳ Generating...', tid, mid);
+
+      let enctxt = encodeURIComponent(txt);
+      let url = `https://arjhil-prodia-api.arjhilbard.repl.co/generate?prompt=${enctxt}&model=${txt2}`;
+
+      let result = await axios.get(url, { responseType: 'arraybuffer' });
+      fs.writeFileSync(path, result.data);
+      api.sendMessage({ attachment: fs.createReadStream(path) }, tid, () => fs.unlinkSync(path), mid);
+    } catch (e) {
+      return api.sendMessage(e.message, tid, mid);
     }
-    await message.unsend(Reply.messageID);
   },
 };
