2af6d2231f146d3389b775a9b8c1b233b1d9010d
JulioRaven <114471308+JulioRaven@users.noreply.github.com>
2024-02-24 18:14:20 +0100

    Update pxart.js

--- scripts/cmds/pxart.js
+++ scripts/cmds/pxart.js
@@ -25,11 +25,10 @@ module.exports = {
     }
   },
   onStart: async function({api, event, args, message }) {
-     api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+    api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
     function r(msg) {
       message.reply(msg);
     }
-    
 
     let g = `•——[Style list]——•\n\n1. Cinematic\n2. Photographic\n3. Anime\n4. Manga\n5. Digital Art\n6. Pixel art\n7. Fantasy art\n8. Neonpunk\n9. 3D Model`;
 
@@ -41,8 +40,19 @@ module.exports = {
     if (!style) return r('Missing style!\n\n' + g);
 
     try {
+      // Vérifier et créer le répertoire cache
+      let cacheDir = path.join(__dirname, 'cache');
+      if (!fs.existsSync(cacheDir)) {
+        fs.mkdirSync(cacheDir, { recursive: true });
+      }
+
+      // Récupérer l'image à partir de l'URL
       const { data } = await get(`${url}/pixart?prompt=${prompt}&styles=${style}`, { responseType: 'arraybuffer' });
+
+      // Écrire l'image dans le fichier pixart.png
       fs.writeFileSync(f, Buffer.from(data, "utf8"));
+
+      // Répondre avec l'image en tant que pièce jointe
       return message.reply({ attachment: fs.createReadStream(f, () => fs.unlinkSync(f)) });
     } catch (e) {
       return message.reply(e.message);
